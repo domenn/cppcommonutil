e517580d82a0f1df9d6e522dff851cd8b14c2a9c
Domen Mori <domen.mory@gmail.com>
2019-10-13 20:37:41 +0200

    Fix SPDLOG, no full file paths, only names. Redo protobuf cmake again.

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -32,48 +32,46 @@ if (DUTILCPP_ADD_PROTOBUF)
 #      endif()
 #    endif()
 
+    set(THE_FILE_LIB_NAME e_protobuf)
+    set(THE_FILE_LIB_VERSION v3.10.0)
+
+    include(ExternalProject)
+
+    ExternalProject_Add(
+        ${THE_FILE_LIB_NAME}
+        PREFIX ${PROJECT_CMK_EXT_PROJS_PRE_CUSTOM}
+        GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
+        GIT_TAG ${THE_FILE_LIB_VERSION}
+        GIT_SHALLOW 1
+        CMAKE_CACHE_ARGS
+        "-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}"
+        "-Dprotobuf_BUILD_TESTS:BOOL=OFF"
+        "-Dprotobuf_BUILD_SHARED_LIBS:BOOL=OFF"
+        "-Dprotobuf_MSVC_STATIC_RUNTIME:BOOL=OFF"
+        "-Dprotobuf_BUILD_EXAMPLES:BOOL=OFF"
+        "-Dprotobuf_WITH_ZLIB:BOOL=OFF"
+        "-DCMAKE_CXX_COMPILER:STRING=${CMAKE_CXX_COMPILER}"
+        "-DCMAKE_INSTALL_PREFIX:PATH=${PROJECT_CMK_EXT_PROJS_PRE_CUSTOM}"
+        # other project specific parameters
+        SOURCE_SUBDIR cmake
+        # BUILD_ALWAYS 1
+        # STEP_TARGETS build
+        EXCLUDE_FROM_ALL true
+    )
+
+    add_dependencies(build_external ${THE_FILE_LIB_NAME})
+    if (${CMK_SCRIPT_VERBOSE})
+      print_external_itm_many(${THE_FILE_LIB_NAME})
+    endif ()
+
+    ExternalProject_Get_Property(${THE_FILE_LIB_NAME} SOURCE_DIR)
+
+    if (${CMK_SCRIPT_VERBOSE})
+      message(STATUS "Protobuf source: ${SOURCE_DIR}")
+    endif ()
+    
     if (NOT protobuf_FOUND)
-
         message(WARNING "Protobuf not found. Run build_external to install it locally. Or install with your package manager.")
-
-        set(THE_FILE_LIB_NAME e_protobuf)
-        set(THE_FILE_LIB_VERSION v3.10.0)
-
-        include(ExternalProject)
-
-        ExternalProject_Add(
-                ${THE_FILE_LIB_NAME}
-                PREFIX ${PROJECT_CMK_EXT_PROJS_PRE_CUSTOM}
-                GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
-                GIT_TAG ${THE_FILE_LIB_VERSION}
-                GIT_SHALLOW 1
-                CMAKE_CACHE_ARGS
-                "-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}"
-                "-Dprotobuf_BUILD_TESTS:BOOL=OFF"
-                "-Dprotobuf_BUILD_SHARED_LIBS:BOOL=OFF"
-                "-Dprotobuf_MSVC_STATIC_RUNTIME:BOOL=OFF"
-                "-Dprotobuf_BUILD_EXAMPLES:BOOL=OFF"
-                "-Dprotobuf_WITH_ZLIB:BOOL=OFF"
-                "-DCMAKE_CXX_COMPILER:STRING=${CMAKE_CXX_COMPILER}"
-                "-DCMAKE_INSTALL_PREFIX:PATH=${PROJECT_CMK_EXT_PROJS_PRE_CUSTOM}"
-                # other project specific parameters
-                SOURCE_SUBDIR cmake
-                # BUILD_ALWAYS 1
-                # STEP_TARGETS build
-                EXCLUDE_FROM_ALL true
-        )
-
-        add_dependencies(build_external ${THE_FILE_LIB_NAME})
-        if (${CMK_SCRIPT_VERBOSE})
-            print_external_itm_many(${THE_FILE_LIB_NAME})
-        endif ()
-
-        ExternalProject_Get_Property(${THE_FILE_LIB_NAME} SOURCE_DIR)
-
-        if (${CMK_SCRIPT_VERBOSE})
-            message(STATUS "Protobuf source: ${SOURCE_DIR}")
-        endif ()
-
     else ()
         if (${CMK_SCRIPT_VERBOSE})
             message(STATUS "Found protobuf: ${protobuf_DIR}")
